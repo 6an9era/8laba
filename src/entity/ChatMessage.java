a568efbdc1b50cc7f1d56833d4a14b0dbcdd519d
Kseniya Ulasovets <kseniyaulasovets@gmail.com>
2023-04-09 12:14:20 +0300

    +B3

--- src/entity/ChatMessage.java
+++ src/entity/ChatMessage.java
@@ -8,15 +8,26 @@ public class ChatMessage {
     private long sec = 60; // было 30 секунд это задание 4
     private long quantity = 15; // это вроде задание 3
     private String badWords ="Сука,сука,Блядь,блядь,Блять,блять,Бля,бля,Ебать,ебать,Нахуй,нахуй";
+    private String HTMLtegs ="<a>,<p>,<body>,<html>,<head>,<div>";
     String[] ErrorWords=badWords.split(",");
+    String[] ErrorHTML = HTMLtegs.split(",");
 
 
     public ChatMessage(String message, ChatUser author, long timestamp) {
         message = Smile(message);
         message = BedWordsFilter(message);
+        message = HTMLFilter(message);
         this.message = message;
         this.author = author;
         this.timestamp = timestamp;
+    }
+    private String HTMLFilter(String message)
+    {
+        for (int i =0;i< ErrorHTML.length;i++)
+        {
+            message=message.replace(ErrorHTML[i],"");
+        }
+        return message;
     }
     private String BedWordsFilter(String message)
     {
